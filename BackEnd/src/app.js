b0cb634a08ebedc25b6d4aaa78d3149df9027a2c
your name <abc@gmail.com>
2023-09-19 04:39:17 +0700

    git commit -m "DN-14 :Crud Category"

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,14 +1,14 @@
-import express from "express"
-import mongoose from "mongoose"
-import productsRouter from "../src/routers/products"
-import authRouter from '../src/routers/auth'
+import express from "express";
+import mongoose from "mongoose";
+import productsRouter from "../src/routers/products";
+import authRouter from "../src/routers/auth";
+import categoryRouter from "../src/routers/category";
 
 const app = express();
 app.use(express.json());
-app.use('/api',productsRouter)
-app.use('/api/',authRouter)
+app.use("/api", productsRouter);
+app.use("/api/", authRouter);
+app.use("/api", categoryRouter);
 
-
-
-mongoose.connect("mongodb://127.0.0.1:27017/DATN")
+mongoose.connect("mongodb://127.0.0.1:27017/DATN");
 export const viteNodeApp = app;
--- BackEnd/src/controllers/products.js
+++ BackEnd/src/controllers/products.js
@@ -1,80 +1,91 @@
 import Products from "../models/products";
 import { productSchema } from "../Schemas/products";
-
+import Category from "../models/category";
 
 export const getAll = async (req, res) => {
-    try {
-        const products = await Products.find()
-        return res.status(200).json({
-            products
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: error
-        })
-    }
-}
-
-
+  try {
+    const products = await Products.find();
+    return res.status(200).json({
+      products,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    });
+  }
+};
 
 export const create = async (req, res) => {
-    try {
-        const {error} = productSchema.validate(req.body)
-        if(error) {
-            return res.status(400).json({
-                message: error.details.map((err)=> err.message)
-            });
-        }
-        const products = await Products.create(req.body)
-        return res.status(200).json({
-            message: "thêm sản phẩm thành công",
-            products
-        })
-    } catch ({ errors }) {
-        return res.status(500).json({
-            message: errors
-        })
+  try {
+    const { error } = productSchema.validate(req.body);
+    if (error) {
+      return res.status(400).json({
+        message: error.details.map((err) => err.message),
+      });
     }
-}
+    const products = await Products.create(req.body);
+    //
+    // Thêm ObjectId vào thuộc tính products trong model Category
+    //
+    await Category.findByIdAndUpdate(products.categoryId, {
+      $addToSet: {
+        product: products._id,
+      },
+    });
+    if (products.length === 0) {
+      return res.status(200).json({
+        message: "Không thêm được sản phẩm",
+      });
+    }
+    return res.json(products);
+  } catch ({ errors }) {
+    return res.status(500).json({
+      message: errors,
+    });
+  }
+};
 
 export const remove = async (req, res) => {
-    try {
-        const products = await Products.findByIdAndDelete({ _id: req.params.id })
-        return res.status(200).json({
-            message: "Xóa sản phẩm thành công"
-        })
-    } catch (errors) {
-        return res.status(500).json({
-            message: errors
-        })
-    }
-}
+  try {
+    const products = await Products.findByIdAndDelete({ _id: req.params.id });
+    return res.status(200).json({
+      message: "Xóa sản phẩm thành công",
+    });
+  } catch (errors) {
+    return res.status(500).json({
+      message: errors,
+    });
+  }
+};
 
 export const getById = async (req, res) => {
-    try {
-        const products = await Products.findById(req.params.id)
-        return res.status(200).json({
-            products
-        })
-    } catch (error) {
-        return res.status(500).json({
-            message: error
-        })
+  try {
+    const products = await Products.findById(req.params.id);
+    return res.status(200).json({
+      products,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error,
+    });
+  }
+};
+export const update = async (req, res) => {
+  try {
+    const products = await Products.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    if (products.length === 0) {
+      return res.status(200).json({
+        message: "Cập nhật sản phẩm không thành công",
+      });
     }
-}
-export const update = async(req ,res)=>{
-    try {
-         const products = await Products.findOneAndUpdate({_id : req.params.id} , req.body,{new: true} );
-         if (products.length === 0) {
-            return res.status(200).json({
-                message: "Cập nhật sản phẩm không thành công",
-            });
-        }
-        return res.json(products);
-    } catch (error) {
-         return  res.status(400).json({
-               message : error.message
-         })
-    }
-   }
-
+    return res.json(products);
+  } catch (error) {
+    return res.status(400).json({
+      message: error.message,
+    });
+  }
+};
