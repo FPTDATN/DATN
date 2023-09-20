024747df91bcda1efa2c8e17625099d58e4473fa
nam <nam@gmail.com>
2023-09-20 13:16:03 +0700

    "DN-39 <[FE]: create Login>"

--- FontEnd/src/App.tsx
+++ FontEnd/src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import {Route, Routes} from 'react-router-dom'
 import Register from './pages/Register/Register'
+import Login from './pages/Login/Login'
 
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
     <>
       <Routes>
         <Route path='signup' element={<Register/>} />
+        <Route path='login' element={<Login/>} />
       </Routes>
     </>
   )
--- FontEnd/src/pages/Login/Login.tsx
+++ FontEnd/src/pages/Login/Login.tsx
@@ -1,30 +1,22 @@
 import React from 'react'
 
-const Register = () => {
+const Login = () => {
     return (
         <div>
             <section className="sigup">
-                <div className="container ">
+                <div className="container mt-5">
                     <div>
                         <div className="container w-full  lg:max-w-xl p-6 space-y-8 sm:p-8 bg-white rounded-lg shadow-xl dark:bg-gray-800">
                             <h2 className=" text-center text-2xl font-bold text-gray-900 dark:text-white">
-                                Đăng Ký
+                                Đăng Nhập
                             </h2>
                             <form className="mt-8 space-y-6" action="#">
-                                <div>
-                                    <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Name</label>
-                                    <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name" required />
-                                </div>
                                 <div>
                                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
                                     <input type="email" name="email" id="email" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required />
                                 </div>
                                 <div>
                                     <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mật khẩu</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
-                                </div>
-                                <div>
-                                    <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nhập lại mật khẩu</label>
                                     <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
                                 </div>
                                 <div className="flex items-start">
@@ -34,11 +26,11 @@ const Register = () => {
                                     <div className="ml-3 text-sm">
                                         <label htmlFor="remember" className="font-medium text-gray-500 dark:text-gray-400">Ghi nhớ</label>
                                     </div>
-
+                                    <a href="#" className="ml-auto text-sm font-medium text-blue-600 hover:underline dark:text-blue-500">Quên mật khẩu</a>
                                 </div>
-                                <button type="submit" className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Đăng ký </button>
+                                <button type="submit" className="w-full px-5 py-3 text-base font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 sm:w-auto dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Đăng nhập </button>
                                 <div className="text-sm font-medium text-gray-900 dark:text-white">
-                                    Đã có tài khoản? <a href="login" className="text-blue-600 hover:underline dark:text-blue-500">đăng nhập tại đây</a>
+                                    Bạn chưa có tài khoản? <a href="signup" className="text-blue-600 hover:underline dark:text-blue-500">đăng ký tại đây</a>
                                 </div>
                             </form>
                         </div>
@@ -49,4 +41,4 @@ const Register = () => {
     )
 }
 
-export default Register
+export default Login
