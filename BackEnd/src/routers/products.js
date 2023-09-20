1d27eb8c2ea5a451be96569940e76080e836bf01
your name <abc@gmail.com>
2023-09-21 02:28:46 +0700

    git commit -m "DN-47 : phân quyền"

--- BackEnd/src/routers/products.js
+++ BackEnd/src/routers/products.js
@@ -1,11 +1,18 @@
 import express from "express";
-import { create, getAll, getById, remove, update } from "../controllers/products";
+import {
+  create,
+  getAll,
+  getById,
+  remove,
+  update,
+} from "../controllers/products";
+import { checkPermission } from "../middlewares/checkPermission";
 
 const router = express.Router();
-router.get('/products', getAll)
-router.get('/products/:id', getById)
-router.delete('/products/:id', remove)
-router.post('/products', create)
-router.patch('/products/:id',update)
+router.get("/products", getAll);
+router.get("/products/:id", getById);
+router.delete("/products/:id", checkPermission, remove);
+router.post("/products", checkPermission, create);
+router.patch("/products/:id", checkPermission, update);
 
-export default router
+export default router;
