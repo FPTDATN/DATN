3218708b05f7c58407a5b4c39f06944a5f1efb87
quangle2003 <quanglek544@gmail.com>
2023-09-22 13:38:55 +0700

    git commit -m "DN-42 <message>"

--- FontEnd/src/App.tsx
+++ FontEnd/src/App.tsx
@@ -2,7 +2,8 @@ import { Route, Routes } from 'react-router-dom'
 import Register from './pages/Register/Register'
 import Login from './pages/Login/Login'
 import Layout from './pages/layout/Layout'
-
+import Pay from './pages/Pay/pay'
+import Card from './pages/Card/card'
 
 function App() {
 
@@ -12,6 +13,10 @@ function App() {
         <Route path='/' element={<Layout />}>
           <Route path='signup' element={<Register />} />
           <Route path='login' element={<Login />} />
+        </Route>
+        <Route>
+        <Route path='pay' element={<Pay />} />
+          <Route path='card' element={<Card />} />
         </Route>
       </Routes>
     </>
