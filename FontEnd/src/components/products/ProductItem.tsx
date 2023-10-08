67407174e7a1608e13262ba0057746b9706a0edd
thanhNguyen2k3 <thanhs2fo4@gmail.com>
2023-10-08 16:52:40 +0700

    [fig-bug]:responsive

--- FontEnd/src/components/products/ProductItem.tsx
+++ FontEnd/src/components/products/ProductItem.tsx
@@ -1,4 +1,4 @@
-import { Card, Rate } from 'antd';
+import { Rate } from 'antd';
 import { FunctionComponent, useState } from 'react';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
@@ -22,13 +22,13 @@ const ProductItem: FunctionComponent<ProductItemProps> = ({ arrangeList }) => {
                 <Skeleton count={4} />
 
             </div> : !arrangeList ? (
-                <Card className="border border-gray-200 rounded-md bg-gray-50 dark:border-gray-900 rounded-b-md dark:bg-gray-900">
+                <div className="border border-gray-200 rounded-md bg-gray-50 dark:border-gray-900 rounded-b-md dark:bg-gray-900">
                     <div className="relative ">
                         <Link to={''} className="">
                             <img
                                 src="https://i.postimg.cc/xC8FnZPT/pexels-suzy-hazelwood-2536965.jpg"
                                 alt=""
-                                className="object-cover w-full mx-auto h-96 lg:h-64"
+                                className="object-cover w-full mx-auto h-32 md:h-80 lg:h-40"
                             />
                         </Link>
                         <div className="absolute z-10 flex items-center justify-center w-10 h-10 p-4 text-center text-gray-100 bg-orange-600 rounded-full shadow-xl left-1 top-1 ">
@@ -40,52 +40,52 @@ const ProductItem: FunctionComponent<ProductItemProps> = ({ arrangeList }) => {
                             </div>
                         </div>
                     </div>
-                    <div className="p-6 ">
-                        <h3 className="mb-3 text-xl font-medium text-center dark:text-gray-400">
+                    <div className="py-6">
+                        <h3 className="mb-3 text-base lg:text-xl line-clamp-2 font-medium text-center dark:text-gray-400">
                             <Link to={''}> Product name</Link>
                         </h3>
                         <p className="mb-3 text-lg font-medium text-center text-gray-600 dark:text-gray-400">
-                            <span className="text-primary/90 dark:text-gray-300">$600.00</span>
-                            <span className="ml-2 text-gray-400 line-through dark:text-gray-400">$1000</span>
+                            <span className="text-primary/90 dark:text-gray-300 text-sm lg:text-xl">$600.00</span>
+                            <span className="ml-2 text-gray-400 line-through dark:text-gray-400 text-sm lg:text-xl">$1000</span>
                         </p>
                         <div className="flex justify-center">
                             <span>
-                                <Rate tooltips={desc} onChange={setValue} value={value} />
+                                <Rate className='text-sm' tooltips={desc} onChange={setValue} value={value} />
                             </span>
                         </div>
                     </div>
-                </Card>
+                </div>
             ) : (
-                <div className="flex border border-gray-200 rounded-md bg-gray-50 dark:border-gray-900 rounded-b-md dark:bg-gray-900">
+                <div className="grid grid-cols-2 border border-gray-200 rounded-md bg-gray-50 dark:border-gray-900 rounded-b-md dark:bg-gray-900">
                     <div className="relative">
                         <Link to={''} className="">
                             <img
                                 src="https://i.postimg.cc/xC8FnZPT/pexels-suzy-hazelwood-2536965.jpg"
                                 alt=""
-                                className="object-cover w-full mx-auto h-60 lg:h-52"
+                                className="object-cover w-full mx-auto h-32 md:h-80 lg:h-52"
                             />
                         </Link>
                         <div className="absolute z-10 flex items-center justify-center w-10 h-10 p-4 text-center text-gray-100 bg-orange-600 rounded-full shadow-xl left-1 top-1 ">
                             <span className="relative text-base font-semibold text-gray-200 "> SALE</span>
                         </div>
                     </div>
-                    <div className="px-6">
-                        <h3 className="mb-3 text-xl font-medium text-center dark:text-gray-400">
+                    <div className="px-6 py-0 md:py-6 lg:py-6">
+                        <h3 className="mb-3 mt-3 text-base lg:text-xl line-clamp-2 font-medium text-center dark:text-gray-400">
                             <Link to={''}> Product name</Link>
                         </h3>
                         <p className="mb-3 text-lg font-medium text-center text-gray-600 dark:text-gray-400">
-                            <span className="text-primary/90 dark:text-gray-300">$600.00</span>
-                            <span className="ml-2 text-gray-400 line-through dark:text-gray-400">$1000</span>
+                            <span className="text-primary/90 dark:text-gray-300 text-sm lg:text-xl">$600.00</span>
+                            <span className="ml-2 text-gray-400 line-through dark:text-gray-400 text-sm lg:text-xl">$1000</span>
                         </p>
                         <div className="flex justify-center">
                             <span>
-                                <Rate tooltips={desc} onChange={setValue} value={value} />
+                                <Rate className='text-sm md:text-base lg:text-xl' tooltips={desc} onChange={setValue} value={value} />
                             </span>
                         </div>
-                        <button className="flex justify-center mt-6 px-4 py-2 text-primary/90 border border-primary/90 rounded-full dark:border-gray-600 dark:text-gray-400 hover:bg-primary/90 hover:text-gray-100 dark:hover:bg-gray-800 dark:hover:border-gray-900">
-                            Add to cart
-                        </button>
                     </div>
+                    <button className="flex w-full col-span-2 text-sm md:text-base lg:text-xl justify-center px-4 py-2 text-primary/90 border border-primary/90 items-center dark:border-gray-600 dark:text-gray-400 hover:bg-primary/90 hover:text-gray-100 dark:hover:bg-gray-800 dark:hover:border-gray-900">
+                        Add to cart
+                    </button>
                 </div>
             )}
 
