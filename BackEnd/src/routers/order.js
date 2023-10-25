5536f64d003de344be6ac0904bf0983099eaecda
nam <nam@gmail.com>
2023-10-25 10:22:34 +0700

    fix bug

--- BackEnd/src/routers/order.js
+++ BackEnd/src/routers/order.js
@@ -1,10 +1,8 @@
 import express from 'express'
 import { checkPermission } from "../middlewares/checkPermission.js";
 import {  createOrder, getOrders, cancelOrder } from '../controllers/order.js';
-import { getRevenueByMonth } from '../statistics/statisicsOrder.js';
 const orderroute = express.Router();
 orderroute.post('/order',createOrder)
 orderroute.get('/order' ,getOrders)
 orderroute.put('/order/:orderId/status',cancelOrder)
-orderroute.get('/orders',getRevenueByMonth)
 export default orderroute
