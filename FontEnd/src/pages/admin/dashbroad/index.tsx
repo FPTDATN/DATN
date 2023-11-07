31a7f19ada16c2147de4e0f05af63766d249fdc8
nam <nam@gmail.com>
2023-11-07 13:41:55 +0700

    [fe]: giao diện thống kê bình luận

--- FontEnd/src/pages/admin/dashbroad/index.tsx
+++ FontEnd/src/pages/admin/dashbroad/index.tsx
@@ -1,14 +1,44 @@
- const Dashbroad = () => {
+import { useGetAllCommentsQuery } from "@/services/comment";
+
+const Dashbroad = () => {
+    const { data } = useGetAllCommentsQuery();
     return (
         <>
             <div className="grid grid-cols-3 gap-4 mb-4">
-                <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
-                    <p className="text-2xl text-gray-400 dark:text-gray-500">
-                        <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
-                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
+                <article
+                    className="flex flex-col gap-4 rounded-lg border border-gray-100 bg-white p-6"
+                >
+                    <div
+                        className="inline-flex gap-2 self-end rounded bg-green-100 p-1 text-green-600"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                stroke-linecap="round"
+                                stroke-linejoin="round"
+                                stroke-width="2"
+                                d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                            />
                         </svg>
-                    </p>
-                </div>
+
+                        <span className="text-xs font-medium"> 67.81% </span>
+                    </div>
+
+                    <div>
+                        <strong className="block text-sm font-medium text-gray-500"> Bình luận </strong>
+
+                        <p>
+                            <span className="text-2xl font-medium text-gray-900"> 1111 </span>
+
+                            <span className="text-xs text-gray-500"> from $240.94 </span>
+                        </p>
+                    </div>
+                </article>
                 <div className="flex items-center justify-center h-24 rounded bg-gray-50 dark:bg-gray-800">
                     <p className="text-2xl text-gray-400 dark:text-gray-500">
                         <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
