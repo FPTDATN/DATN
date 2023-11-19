11fc969578c538ed8f87dfcf87fded297eb1a1a5
nam <nam@gmail.com>
2023-11-20 01:35:10 +0700

    [Be]: thống kê đơn hàng

--- FontEnd/src/pages/admin/dashbroad/index.tsx
+++ FontEnd/src/pages/admin/dashbroad/index.tsx
@@ -154,6 +154,7 @@ const Dashbroad = () => {
 
                 </article>
             </div>
+
             <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
                 <p className="text-2xl text-gray-400 dark:text-gray-500">
                     <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
