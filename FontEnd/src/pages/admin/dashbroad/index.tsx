b8fd9bb3fb859b6e76cf3fd27f3e9305d8b87259
your name <abc@gmail.com>
2023-11-20 21:09:56 +0700

    push lại tk đơn hàng

--- FontEnd/src/pages/admin/dashbroad/index.tsx
+++ FontEnd/src/pages/admin/dashbroad/index.tsx
@@ -1,6 +1,6 @@
 import { useLogoutMutation, useMeQuery } from "@/services/auth";
 import { useGetAccountCommentsQuery } from "@/services/comment";
-import { useGetRevenueStatisticsQuery } from "@/services/order";
+import { useGetOrderStatisticsQuery, useGetRevenueStatisticsQuery } from "@/services/order";
 import { useGetTotalProductQuery } from "@/services/product";
 import { useGetAccountQuery } from "@/services/user";
 
@@ -9,10 +9,12 @@ const Dashbroad = () => {
     const { data: DataUser } = useGetAccountQuery()
     const { data: revenueData } = useGetRevenueStatisticsQuery()
     const { data: totalProduct } = useGetTotalProductQuery()
+    const {data:DataOrders} = useGetOrderStatisticsQuery()
     const usage = DataUser?.usage ?? 0;
     const totalComments = DataComment?.totalComments ?? 0;
     const totalP = totalProduct?.total ?? 0;
     const totalRevenue = revenueData?.totalRevenue ?? 0;
+    const totalTotal = DataOrders?.totalOrders ?? 0;
     const { data: authData } = useMeQuery();
     const isAdmin = authData?.role === 'admin';
     return (
@@ -154,12 +156,49 @@ const Dashbroad = () => {
 
                 </article>
             </div>
-            <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
+            {/* <div className="flex items-center justify-center h-48 mb-4 rounded bg-gray-50 dark:bg-gray-800">
                 <p className="text-2xl text-gray-400 dark:text-gray-500">
                     <svg className="w-3.5 h-3.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 18">
                         <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 1v16M1 9h16" />
                     </svg>
                 </p>
+            </div> */}
+            <div className="grid grid-cols-4 gap-4 mb-4">
+                <article
+                    className="flex flex-col gap-4 rounded-lg border border-gray-100 bg-white p-6"
+                >
+                    <div
+                        className="inline-flex gap-2 self-end rounded bg-green-100 p-1 text-green-600"
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            className="h-4 w-4"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            stroke="currentColor"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M13 7h8m0 0v8m0-8l-8 8-4-4-6 6"
+                            />
+                        </svg>
+
+                        {/* <span className="text-xs font-medium"> 67.81% </span> */}
+                    </div>
+
+                    <div>
+                        <strong className="block text-sm font-medium text-gray-500"> Tổng đơn hàng </strong>
+
+                        <p>
+                            <span className="text-2xl font-medium text-gray-900"> {totalTotal}</span>
+
+                            <span className="text-xs text-gray-500"> đơn hàng </span>
+                        </p>
+                    </div>
+
+                </article>
             </div>
             <div className="grid grid-cols-2 gap-4 mb-4">
                 <div className="flex items-center justify-center rounded bg-gray-50 h-28 dark:bg-gray-800">
